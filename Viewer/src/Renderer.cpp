8de0914fd5dc88b085d6a296ae7d6e77c1311f0d
Elias <elias.jadon1@gmail.com>
2018-10-28 16:07:02 +0200

    bresenham: cases
    wrote a function that draw line

--- Viewer/src/Renderer.cpp
+++ Viewer/src/Renderer.cpp
@@ -342,7 +342,7 @@ void Renderer::Render(const Scene& scene, const ImGuiIO& io)
 	q2 = io.MousePos.y;
 	printf("viewportHeight = %f", viewportHeight);
 	printf("p1 = (%f,%f) , p2 = (%f,%f)\n", p1, q1, p2, q2);
-	DrawLine(p1, p2, q1, q2);
+	NaiveAlg(p1, p2, q1, q2);
 }
 
 //##############################
