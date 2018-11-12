f4de73116c92a6c5a49779636fb0c4d81b8ffa74
Elias <elias.jadon1@gmail.com>
2018-11-12 21:37:15 +0200

    pre-connection to master at first time

--- Viewer/src/Camera.cpp
+++ Viewer/src/Camera.cpp
@@ -16,7 +16,16 @@ Camera::~Camera()
 
 void Camera::SetCameraLookAt(const glm::vec3& eye, const glm::vec3& at, const glm::vec3& up)
 {
-
+	glm::vec4 eye4 = glm::vec4(eye[0], eye[1], eye[2],1);
+	glm::vec4 at4 = glm::vec4(at[0], at[1], at[2], 1);
+	glm::vec4 up4 = glm::vec4(up[0], up[1], up[2], 1);
+	
+	
+	glm::vec4 z = normalize(eye4 - at4);
+	//glm::vec4 x = cross(up4, z);
+	//glm::vec4 y =
+	//glm::vec4 t =
+	//glm::vec4 c =
 }
 
 void Camera::SetOrthographicProjection(
